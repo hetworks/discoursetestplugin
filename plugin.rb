9cff6094d3c022e00364b45017e18e0fe6b0c289
annerozema <33345841+annerozema@users.noreply.github.com>
2018-08-21 13:56:13 +0200

    dingen testen

--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,17 @@
 register_javascript <<JS
 $(document).ready(function() {
     console.log('js test in plugin.rb');
-    $(".controls").append("<p>Hier een blokje dingetjes</p>");
+    var categories = [];
+    $("table.category-list tbody tr").each(function() {
+        var cat = {};
+        var cattd = $(this).find('td').first();
+        cat.color = cattd.css("border-left-color");
+        cat.href = cattd.find('h3 a').attr('href');
+        cat.name = cattd.find('h3 span.category-name').html();
+        cat.description = cattd.find('div.category-description').html();
+        categories.push(cat);
+    });
+    console.log(categories);
+    $("body.navigation-categories .controls").append("<p>Hier een blokje dingetjes</p>");
 });
 JS
