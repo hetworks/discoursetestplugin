408b62d32eb388f0cda93966e0dc74d2569328a7
annerozema <33345841+annerozema@users.noreply.github.com>
2018-08-21 14:09:59 +0200

    dingen testen

--- plugin.rb
+++ plugin.rb
@@ -10,13 +10,22 @@ $(document).ready(function() {
     $("table.category-list tbody tr").each(function() {
         var cat = {};
         var cattd = $(this).find('td').first();
+        cat.id = $(this).attr('data-category-id');
         cat.color = cattd.css("border-left-color");
         cat.href = cattd.find('h3 a').attr('href');
-        cat.name = cattd.find('h3 span.category-name').html();
-        cat.description = cattd.find('div.category-description').html();
+        cat.name = cattd.find('h3 span.category-name').text();
+        cat.image = cattd.find('img.category-logo');
+        cat.description = cattd.find('div.category-description').text();
         categories.push(cat);
     });
     console.log(categories);
-    $("body.navigation-categories .controls").append("<p>Hier een blokje dingetjes</p>");
+    $.each(categories, function(i,v) {
+        $("body.navigation-categories .controls").append("<div class='cattegel-"+v.id+"'><a href='"+v.href+"'></a></div>");
+        cattegel = $("div.cattegel-"+v.id+" a");
+        cattegel.css("border-left-color", cat.color);
+        cattegel.append("<h3>"+v.name+"</h3>");
+        cattegel.append("<img src='"+v.image+"' />");
+    });
+
 });
 JS
